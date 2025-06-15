e75f1ff5f67dcb965aef682a734ee949f1eb7def
ammarudink <ammarudin.kovacevic@stu.ibu.edu.ba>
2025-06-15 22:27:43 +0200

    fixes

--- login/utils/constants.js
+++
@@ -1,9 +0,0 @@
-var Constants = {
-    get_api_base_url: function () {
-      if (location.hostname == "localhost") {
-          return "http://localhost/WebProgramming/";
-      } else {
-          return "electro-bcqmb.ondigitalocean.app";
-      }
-    },
-  };
